b22ca6dc0a532740915c85e4e1eb376b7d1bb6b7
Mohammed Hassan <muhammad.hassan@ozbul.com>
2021-05-21 00:15:44 +0500

    Set up entity and repository projects

--- InvoiceGenerator.Repository/Migrations/00000000000000_CreateIdentitySchema.cs
+++ InvoiceGenerator.Repository/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace InvoiceGenerator.Data.Migrations
+namespace InvoiceGenerator.Repository.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
