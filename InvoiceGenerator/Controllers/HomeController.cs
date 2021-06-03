2f7b006b8d867324fd5082ff9330bcf8d77395d2
Iverique Nkayilu <iverique.mbungu@ozbul.com>
2021-06-03 07:39:54 +0200

    login and logout backend

--- InvoiceGenerator/Controllers/HomeController.cs
+++ InvoiceGenerator/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using InvoiceGenerator.Models;
+﻿using InvoiceGenerator.Entities;
+using InvoiceGenerator.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,16 +14,43 @@ namespace InvoiceGenerator.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<User> _signInManager;
+        public HomeController(ILogger<HomeController> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel input)
+        {
+            if (input == null) return View();
+            if (string.IsNullOrEmpty(input.Username)) return View();
+            if (string.IsNullOrEmpty(input.Password)) return View();
+
+            var result = await _signInManager.PasswordSignInAsync(input.Username, input.Password, true, false);
+            if (result.Succeeded)
+                return RedirectToAction(nameof(Index));
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login)); // test, or add "Home", action, controller
+        }
+
 
         public IActionResult Privacy()
         {
