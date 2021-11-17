a585dc4b1a48c98fdd5ceb70a6972733dd99e3e3
mhb96 <36849769+mhb96@users.noreply.github.com>
2021-11-18 00:19:06 +0500

    Added theme files (#7)
    Co-authored-by: Mohammed Hassan <muhammad.hassan@ozbul.com>

---
+++ InvoiceGenerator/wwwroot/js/login.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+app.controller("login",
+    function ($scope, $http) {
+        $scope.init = function () {
+            console.log("123");
+            $scope.username = null;
+            $scope.password = null;
+        }
+
+        $scope.login = function () {
+            var model = {
+                Username : $scope.username,
+                Password : $scope.password
+            };
+            $http({
+                method: "POST",
+                url: `/home/login`,
+                data: model
+            }).then(function () {},
+                function (error) {
+                    alert('error', `Invalid user credentials.`);
+             });
+        }
+    }
+);
